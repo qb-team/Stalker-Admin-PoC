502cf9188ab739912d8a392c87ab485755b2d0f7
federico.p98@outlook.it <53578288+pheasant98@users.noreply.github.com>
2020-03-02 23:16:59 +0100

    #5 aggiunto funzionalità informazioni generali dell'organizzazione

--- src/app/control-panel/content/content-track-users/content-track-users-general-informations/content-track-users-general-informations.component.ts
+++ src/app/control-panel/content/content-track-users/content-track-users-general-informations/content-track-users-general-informations.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { Organization } from 'src/model/models';
+import { Address } from 'src/model/models';
 
 @Component({
   selector: 'app-content-track-users-general-informations',
